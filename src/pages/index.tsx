9cbd12802d63475b47abebf033584bf720666352
sravan2305 <gollapudivenkatasravankumar@gmail.com>
2022-03-08 21:58:37 +0530

    unused vars removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable unused-imports/no-unused-vars */
-import { useRouter } from 'next/router';
 
 import { Convert } from '@/components/Convert';
 import { Hero } from '@/components/Hero';
 import { WhyUs } from '@/components/WhyUs';
 
 const Index = () => {
-  const router = useRouter();
-
   return (
     <main className="overflow-x-hidden flex-col center-block">
       <Hero />
