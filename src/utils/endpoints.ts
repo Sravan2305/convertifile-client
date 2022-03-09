a3eb1708d8f36f5b9d03e3e4ec320c52f041f440
sravan2305 <gollapudivenkatasravankumar@gmail.com>
2022-03-09 15:59:44 +0530

    updated endpoints

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,6 +1,6 @@
 // const DOMAIN = process.env.DOMAIN ?? `http://127.0.0.1:8000`;
 
-const DOMAIN = `http://6016-2405-201-c022-504b-840a-9285-9a7-beb8.ngrok.io/`;
+const DOMAIN = `https://6016-2405-201-c022-504b-840a-9285-9a7-beb8.ngrok.io`;
 
 export const API_ENDPOINT = `${DOMAIN}/app/v1/convert`;
 
